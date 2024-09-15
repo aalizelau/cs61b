60895cb662a752011322803359a4666fda21c028
Aalizelau <aalizelau@gmail.com>
2024-09-15 13:11:16 +0800

    project 2B basic

--- proj2b/src/main/Graph.java
+++ proj2b/src/main/Graph.java
@@ -1,4 +1,69 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Graph {
+    private final int VERTEXNO;
+    private int source;
+    private List<Integer>[] adj;
+    private boolean[] marked;
+    private int[] edgeTo;
+
+
+    public Graph(int vertex) {
+        this.VERTEXNO = vertex;
+        adj = (List<Integer>[]) new ArrayList[vertex];
+
+        for (int v = 0; v < vertex; v++) {
+            adj[v] = new ArrayList<Integer>();
+        }
+    }
+
+    //add an edge of v pointing towards w
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    public Iterable<Integer> neighbor(int v) {
+        return adj[v];
+    }
+
+    public int getVERTEXNO(){
+        return VERTEXNO;
+    }
+
+    public void DepthFirstPaths(int source) {
+        marked = new boolean[VERTEXNO]; // assuming Graph has a method vertex() to get the number of vertices
+        edgeTo = new int[VERTEXNO];
+        this.source = source;
+        dfsHelper(source);
+    }
+
+    private void dfsHelper(int v) {
+        marked[v] = true;
+        for (int w : neighbor(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfsHelper(w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;  // No path if vertex is not connected
+
+        Stack<Integer> path = new Stack<Integer>();
+
+        for (int x = v; x != source; x = edgeTo[x]) {
+            path.push(x);
+        }
+        path.push(source);
+        return path;
+    }
 }
--- proj2b/src/main/WordNet.java
+++ proj2b/src/main/WordNet.java
@@ -1,9 +1,51 @@
 package main;
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordNet {
     private Graph graph;
+    private Map<Integer, String[]> wordMap;
 
-    public WordNet(){
-        graph = new Graph();
+    public WordNet(String synsetFilename, String hyponymFilename){
+        int lineCount = loadSynsetFile(synsetFilename);
+        loadHyponymFile(hyponymFilename);
+
+        graph = new Graph(lineCount);
+        wordMap = new HashMap<>();
+    }
+
+    private void loadHyponymFile(String hyponymFilename) {
+        In hyponymIn = new In(hyponymFilename);
+
+        while (!hyponymIn.isEmpty()) {
+            String nextLine = hyponymIn.readLine();
+            String[] splitLine = nextLine.split(",");
+            int parentNode = Integer.parseInt(splitLine[0]);
+
+            for (int i = 1; i < splitLine.length; i++) {
+                int neighborNode = Integer.parseInt(splitLine[i]);
+                graph.addEdge(parentNode, neighborNode);
+            }
+        }
+    }
+
+    private int loadSynsetFile(String synsetFilename) {
+        In synsetIn = new In(synsetFilename);
+        int lineCount = 0;
+
+        while (!synsetIn.isEmpty()) {
+            String nextLine = synsetIn.readLine();
+            String[] splitLine = nextLine.split(",");
+            int synsetID = Integer.parseInt(splitLine[0]);
+            String synset = splitLine[1];
+
+            String[] synsetList = synset.split(" ");
+            wordMap.put(synsetID,synsetList);
+            lineCount++;
+        }
+        return lineCount;
     }
 }
